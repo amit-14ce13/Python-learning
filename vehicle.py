e8bdd557c896b259dd72fd4954e0ee3146ad98c1
amit-14ce13 <amit.14ce13@gmail.com>
2022-08-16 13:14:04 +0530

    "oops exercise1"

---
+++ vehicle.py
@@ -0,0 +1,11 @@
+class Vehicle1:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+modelx = Vehicle1(240, 18)
+
+print(modelx.max_speed, modelx.mileage)
+
+
